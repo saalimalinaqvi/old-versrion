4d8c7e3ccc57205037267edd7d7eb7d17010c7de
rajkumarg6 <rajkumarganesan6399@gmail.com>
2025-02-19 22:51:05 +0530

    Game Assets uploaded
    Assets, model, materials, prefabs, scenes & scripts are attached

---
+++ Events.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    public float maxSpeed;
+
+    private int desiredLane = 1;
+    public float laneDistance = 4;
+
+    public float jumpForce;
+    public float Gravity = -20;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forwardSpeed < maxSpeed)
+            forwardSpeed += 0.1f * Time.deltaTime;
+        direction.z = forwardSpeed;
+        
+        if (controller.isGrounded)
+        {
+            direction.y = -2;
+            if (SwipeManager.swipeUp)
+            {
+                Jump();
+            }
+        }
+       else
+        {
+            direction.y += Gravity * Time.deltaTime;
+        }
+
+        if(SwipeManager.swipeRight)
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+        if (SwipeManager.swipeLeft)
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (desiredLane ==0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+
+        else if (desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        if (transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+            controller.Move(moveDir);
+        else
+            controller.Move(diff);
+    }
+    private void FixedUpdate()
+    {
+        if (!PlayerManager.isGameStarted)
+            return;
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "Obstacle")
+        {
+            PlayerManager.gameOver = true;
+        }
+    }
+}
